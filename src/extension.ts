65019188df17f6cb46bab4acb89aadf4f0c7ceec
BHznJNs <441768875@qq.com>
2025-01-05 18:46:44 +0800

    removed async for activate hook

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,12 @@ import {
     registerImportResourcesCommand,
 } from './commands';
 
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
     registerTerminalCommand(context);
     registerSummaryCommand(context);
     registerImportResourcesCommand(context);
 
-    await vscode.commands.executeCommand('markdown-blog-ext.preview');
+    vscode.commands.executeCommand('markdown-blog-ext.preview');
 }
 
 export function deactivate() {}
